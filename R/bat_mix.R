2cd717cde2812578c64dc87e88ea2e2b33a704b0
Kees Mulder <keestimmulder@gmail.com>
2019-01-14 15:24:27 +0100

    Batschelet Mixtures

---
+++ R/bat_mix.R
@@ -0,0 +1,134 @@
+
+
+print.bat_mix_mod <- function(x, digits = 3, ...) {
+  print(round(coef(x), digits))
+}
+
+
+coef.bat_mix_mod <- coefficients.bat_mix_mod <- function(x, ...) {
+  coef_mat <- x$mcmc_summary
+  coef_mat
+}
+
+
+posterior_samples.bat_mix_mod <- function(x) {
+  mat <- x$mcmc_sample[, 1:(x$n_components * 4)]
+  mat
+}
+
+
+plot.bat_mix_mod <- function(x, ...) {
+  mu_names    <- x$mu_names
+  kp_names    <- x$kp_names
+  lam_names   <- x$lam_names
+  alph_names  <- x$alph_names
+
+  if (x$bat_type == "inverse") {
+    pdf_fun <- function(x, params) flexcircmix::dbatmix(x, dbat_fun = dinvbat,
+                                                        mus   = params[mu_names  ],
+                                                        kps   = params[kp_names  ],
+                                                        lams  = params[lam_names ],
+                                                        alphs = params[alph_names])
+  } else if (x$bat_type == "power") {
+    pdf_fun <- function(x, params) flexcircmix::dbatmix(x, dbat_fun = dpowbat,
+                                                        mus   = params[mu_names  ],
+                                                        kps   = params[kp_names  ],
+                                                        lams  = params[lam_names ],
+                                                        alphs = params[alph_names])
+  }
+
+  plot_circbayes_univariate(x, pdf_fun = pdf_fun, ...)
+}
+
+
+
+
+#' @importFrom bridgesampling bridge_sampler
+bridge_sampler.bat_mix_mod <- function(samples, ...) {
+
+    if (samples$method != "bayes") {
+      stop("Bridge sampling is only possible for method =='bayes'.")
+    }
+
+    n_comp <- samples$n_components
+
+    sam <- samples$mcmc_sample[, 1:(4*n_comp)]
+
+    lb <- rep(c(-2*pi, 0, -1, 0), each = n_comp)
+    ub <- rep(c(2*pi, Inf, 1, 1), each = n_comp)
+
+    names(lb) <- colnames(sam)
+    names(ub) <- colnames(sam)
+
+    bridgesampling::bridge_sampler(data = samples$x,
+                                   samples = as.matrix(sam),
+                                   param_types = rep(c("circular", "real", "real", "simplex"), each = n_comp),
+                                   log_posterior = samples$log_posterior,
+                                   lb = lb, ub = ub,
+                                   silent = TRUE, ...)
+}
+
+marg_lik.bat_mix_mod <- function(x, ...) {
+
+  bsobj <- bridgesampling::bridge_sampler(samples = x,
+                                          ...)
+
+  bridgesampling::logml(bsobj)
+}
+
+
+inf_crit.bat_mix_mod <- function(x, ...) {
+  x$ic_mat
+}
+
+
+#' Posterior of mixture of Power or Inverse Batschelet distributions.
+#'
+#' @param th Circular observations, either \code{numeric} in radians, or
+#'   \code{circular}.
+#' @param niter Number of iterations to perform MCMC for.
+#' @param ... Further arguments passed to \code{circglmbayes::fitbatmix}.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' bat_mix(rvm(30, 2, 5))
+#'
+bat_mix <- function(th,
+                    bat_type = "power",
+                    n_comp = 2,
+                    mu_logprior_fun  = function(mu)  0,
+                    kp_logprior_fun  = function(kp)  0,
+                    lam_logprior_fun = function(lam) 0,
+                    niter = 1000, ...) {
+
+  th <- as.circrad(th)
+
+  # Run single component Power or Inverse Batschelet mixture model.
+  res <- flexcircmix::fitbatmix(x = th, method = "bayes", Q = niter,
+                                n_comp = n_comp,
+                                bat_type = bat_type,
+                                mu_logprior_fun  = mu_logprior_fun,
+                                kp_logprior_fun  = kp_logprior_fun,
+                                lam_logprior_fun = lam_logprior_fun, ...)
+
+  coef_batpost <- coef(res)
+
+  # dbat_fun <- ifelse(bat_type == "inverse", dinvbat, dpowbat)
+
+
+
+  batpost_res <- c(list(coef = coef_batpost),
+                   res,
+                   list(data = res$x))
+
+  batpost_res$mu_names   <- paste0("mu_", seq(n_comp))
+  batpost_res$kp_names   <- paste0("kp_", seq(n_comp))
+  batpost_res$lam_names  <- paste0("lam_", seq(n_comp))
+  batpost_res$alph_names <- paste0("alph_", seq(n_comp))
+
+  class(batpost_res) <- c("bat_mix_mod", class(res))
+
+  batpost_res
+}
